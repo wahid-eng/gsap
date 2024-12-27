3fe224a54493df79e08aeca971b2292ec90f7d72
Mohammad Wahid <mohammadwahid.eng@gmail.com>
2024-12-27 08:10:11 +0600

    scroll animation added

--- scripts.js
+++ scripts.js
@@ -1,19 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
-	gsap.set('h1', { opacity: 0, scale: 0 });
-
-	const tl = gsap.timeline();
-	tl.from('header > a', {
-		y: -20,
-		opacity: 0,
-	})
-		.from('nav li', {
-			x: 20,
-			opacity: 0,
-			stagger: 0.1,
-		})
-		.to('h1', {
-			scale: 2,
-			opacity: 1,
-			duration: 1,
-		});
+	gsap.to('section h2', {
+		transform: 'translateX(-150%)',
+		scrollTrigger: {
+			trigger: '.overflow-hidden',
+			markers: true,
+			start: 'top 0%',
+			scrub: true,
+			pin: true,
+		},
+	});
 });
